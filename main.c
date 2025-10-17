5983b4e62473aca8d82d599174f6060f44703b8a
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-10-17 14:16:54 +0900

    Week07_03

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int inc(int counter);
+ 
 int main(int argc, char *argv[])
 {
-  int i;
-  
-  for (i=0;i<5;i++)
-  {
-      int temp =1; //원하는 값(1,2,3,4,5) 나오려면 static 넣거나 for문 바깥에 temp변수 설정 
-      printf("temp =%d\n", temp);
-      temp++;
-  } 
+  int i=10;
+  printf("함수 호출전 i=%d\n", i);
+  inc(i);
+  printf("함수 호출후 i=%d\n", i);
+   
   system("PAUSE");	
   return 0;
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;    //매개변수 또한 지역변수이므로 11 아니고 10으로 나옴. 
 }
