ad04e25f0e313e39b51af2e8a37207786e6f9643
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-10-17 14:25:08 +0900

    Week07_04

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int inc(int counter);
+void f(void);
+int i;
+
  
 int main(int argc, char *argv[])
 {
-  int i=10;
-  printf("함수 호출전 i=%d\n", i);
-  inc(i);
-  printf("함수 호출후 i=%d\n", i);
-   
+  for( i=0; i<5; i++)
+  {
+       f();
+  }
   system("PAUSE");	
   return 0;
 }
 
-int inc(int counter)
+void f(void)
 {
-    counter++;
-    return counter;    //매개변수 또한 지역변수이므로 11 아니고 10으로 나옴. 
+     for(i=0;i<10;i++)  
+       printf("#");
 }
+
+//#이 50개가 아니라 10개만 찍힘 => i 전역변수라서, 50개 하려면 지역변수로 설정해줘야됨 
